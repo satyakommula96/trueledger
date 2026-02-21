b25419c0f626ecf5cc2bfcead70a258c2cda4a92
Satya Kommula <satyakommula96@gmail.com>
2026-02-21 12:46:31 +0530

    release: v1.5.0+15

--- lib/core/config/version.dart
+++ lib/core/config/version.dart
@@ -1,6 +1,6 @@
 class AppVersion {
   // App Version (Semantic Versioning) - Displayed in Settings
-  static const String current = '1.4.3';
+  static const String current = '1.5.0';
 
   // Database Version (Integer) - Used for SQLite Migrations
   // Increment this ONLY when you modify the database schema (db/schema.dart)
